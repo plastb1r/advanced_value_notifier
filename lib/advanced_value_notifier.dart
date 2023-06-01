280797ff0f81b7c8ce020b3e12db7dadcdf1f94c
plastb1r <eremin@surfstudio.ru>
2023-06-01 23:06:28 +0600

    fix: Add file exports

--- lib/advanced_value_notifier.dart
+++ lib/advanced_value_notifier.dart
@@ -1,3 +1,9 @@
 library advanced_value_notifier;
 
-export 'advanced_value_notifier.dart';
+export 'src/advanced_value.dart';
+export 'src/advanced_value_notifier.dart';
+
+export 'src/builders/advanced_value_listenable_builder.dart';
+export 'src/builders/double_value_listenable_builder.dart';
+export 'src/builders/triple_value_listenable_builder.dart';
+export 'src/builders/multi_listenable_rebuilder.dart';
